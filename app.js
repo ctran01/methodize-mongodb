bd01036b049e4d4408ce9885d837e729ef99d006
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2021-01-19 23:49:28 -0600

    MongoDb added and connected on atlas

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
+const { mongoURI } = require("./config/keys");
 const app = express();
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    newCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send({ Message: "Sucessfully connected!" });
